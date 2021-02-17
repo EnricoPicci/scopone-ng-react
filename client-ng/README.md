3f0a7fd581325e2a62afb733840b74bd3700c867
Enrico PIccinin <enrico.piccinin@gmail.com>
2021-02-17 11:44:20 +0100

    rename client to client-ng and add client-react

--- client-ng/README.md
+++ client-ng/README.md
@@ -1,4 +1,4 @@
-# Client
+# Client-ng
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.4.
 
--- client-ng/src/app/app.component.spec.ts
+++ client-ng/src/app/app.component.spec.ts
@@ -16,10 +16,10 @@
 //     expect(app).toBeTruthy();
 //   });
 
-//   it(`should have as title 'client'`, () => {
+//   it(`should have as title 'client-ng'`, () => {
 //     const fixture = TestBed.createComponent(AppComponent);
 //     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('client');
+//     expect(app.title).toEqual('client-ng');
 //   });
 
 //   it('should render title', () => {
@@ -27,7 +27,7 @@
 //     fixture.detectChanges();
 //     const compiled = fixture.nativeElement;
 //     expect(compiled.querySelector('.content span').textContent).toContain(
-//       'client app is running!'
+//       'client-ng app is running!'
 //     );
 //   });
 // });
