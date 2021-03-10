77f78f5abb49bfb68b763d62b2413733a02b90bb
Enrico PIccinin <enrico.piccinin@gmail.com>
2021-03-10 13:15:42 +0100

    teams added to table

--- client-react/src/components/hand/hand.tsx
+++ client-react/src/components/hand/hand.tsx
@@ -1,10 +1,51 @@
-import React, { FC } from "react";
+import React, { FC, useContext, useEffect, useState } from "react";
+import { merge } from "rxjs";
+import { tap } from "rxjs/operators";
+import { ServerContext } from "../../context/server-context";
+import { HandState, Team } from "../../rx-services/scopone-rx-service/messages";
 import { Table } from "../table/table";
 
 export const Hand: FC = () => {
+  const server = useContext(ServerContext);
+  // const errorService = useContext(ErrorContext);
+
+  // const history = useHistory();
+
+  const [showStartButton, setShowStartButton] = useState(false);
+  const [teams, setTeams] = useState<[Team, Team]>(null);
+
+  useEffect(() => {
+    console.log("=======>>>>>>>>>>>>  Use Effect run in Hand");
+
+    // myCurrentGame$ Observable manages set teams and showSartButton state as a side effect
+    // when the updated info about my current game is notified on the server stream
+    const myCurrentGame$ = server.myCurrentOpenGame_ShareReplay$.pipe(
+      tap((game) => {
+        setTeams(game.teams);
+        // decide whether to show or not the Start Game button
+        const gameWith4PlayersAndNoHand =
+          Object.keys(game.players).length === 4 && game.hands.length === 0;
+        const lastHandClosed = game.hands
+          ? game.hands.length > 0
+            ? game.hands[game.hands.length - 1].state === HandState.closed
+            : false
+          : false;
+        setShowStartButton(gameWith4PlayersAndNoHand || lastHandClosed);
+      })
+    );
+
+    const subscription = merge(myCurrentGame$).subscribe();
+
+    return () => {
+      console.log("Unsubscribe Hand subscription");
+      subscription.unsubscribe();
+    };
+  }, [server]);
   return (
     <>
-      <Table></Table>
+      {teams && (
+        <Table teams={teams} currentPlayerName={server.playerName}></Table>
+      )}
     </>
   );
 };
--- client-react/src/components/table/table.tsx
+++ client-react/src/components/table/table.tsx
@@ -1,8 +1,34 @@
 import { FC } from "react";
+import {
+  PlayerState,
+  Team,
+} from "../../rx-services/scopone-rx-service/messages";
 
 import "./table.css";
 
-export const Table: FC = () => {
+interface ITableProps {
+  teams: [Team, Team];
+  currentPlayerName: string;
+}
+
+export const Table: FC<ITableProps> = ({ teams, currentPlayerName }) => {
+  const playerName = (tNum: number, pNum: number) => {
+    let name = teams[tNum].Players[pNum] ? teams[tNum].Players[pNum].name : "-";
+    name = playerLeft(tNum, pNum) ? `${name} (left the Osteria)` : name;
+    name = isCurrentPlayer(tNum, pNum) ? `${name} (can play card)` : name;
+    return name;
+  };
+  const playerLeft = (tNum: number, pNum: number) => {
+    return teams && teams[tNum].Players[pNum]
+      ? teams[tNum].Players[pNum].status === PlayerState.playerLeftTheGame
+      : false;
+  };
+  const isCurrentPlayer = (tNum: number, pNum: number) => {
+    return teams && teams[tNum].Players[pNum]
+      ? teams[tNum].Players[pNum].name === currentPlayerName
+      : false;
+  };
+
   return (
     <>
       <div className="main">
@@ -13,23 +39,23 @@ export const Table: FC = () => {
                 <tr className="table-row">
                   <td></td>
                   <td>
-                    <div className="player player-3">playerName 1_0</div>
+                    <div className="player player-3">{playerName(1, 0)}</div>
                   </td>
                   <td></td>
                 </tr>
                 <tr className="table-row">
                   <td>
-                    <div className="player player-1">playerName 0_0</div>
+                    <div className="player player-1">{playerName(0, 0)}</div>
                   </td>
                   <td></td>
                   <td>
-                    <div className="player player-2">playerName 0_1</div>
+                    <div className="player player-2">{playerName(0, 1)}</div>
                   </td>
                 </tr>
                 <tr className="table-row">
                   <td></td>
                   <td>
-                    <div className="player player-4">playerName 1_1</div>
+                    <div className="player player-4">{playerName(1, 1)}</div>
                   </td>
                   <td></td>
                 </tr>
