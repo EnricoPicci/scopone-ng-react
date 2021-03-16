5674b09d5804f138dc5ada8aa9686db31146a2d8
Enrico PIccinin <enrico.piccinin@gmail.com>
2021-03-16 10:43:26 +0100

    ThemeProvider  to avoid Material Dialogue warning

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
 import { BrowserRouter } from "react-router-dom";
+import {
+  ThemeProvider,
+  unstable_createMuiStrictModeTheme,
+} from "@material-ui/core/styles";
 
 import "./App.css";
 import { Game } from "./components/game/game";
 
+// ThemeProvider added to avoid "Warning: findDOMNode is deprecated in StrictMode" issued with Material Dialogue
+// https://stackoverflow.com/a/64135466/5699993
+const theme = unstable_createMuiStrictModeTheme();
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Game></Game>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Game></Game>
+        </div>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
